8d216c4664cd8d5217d037b5b939f53d2d905dfc
sandip.p.sangale <sandip.p.sangale@CPX-8TJR5EKX6XS.dir.svc.accenture.com>
2019-08-15 14:26:08 +0100

    Working request

--- src/main/java/com/techprimers/springbootsoapexample/endpoint/UserEndpoint.java
+++ src/main/java/com/techprimers/springbootsoapexample/endpoint/UserEndpoint.java
@@ -1,16 +1,18 @@
 package com.techprimers.springbootsoapexample.endpoint;
 
-import com.techprimers.spring_boot_soap_example.GetUserRequest;
-import com.techprimers.spring_boot_soap_example.GetUserResponse;
-import com.techprimers.springbootsoapexample.service.UserService;
-
-import uk.gov.gsi.childmaintenance.futurescheme.bo.esb.eportal.cmecgetclientdetailsrequest.CmecGetClientDetailsRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.techprimers.spring_boot_soap_example.GetUserRequest;
+import com.techprimers.spring_boot_soap_example.GetUserResponse;
+import com.techprimers.springbootsoapexample.service.UserService;
+
+import uk.gov.gsi.childmaintenance.futurescheme.bo.esb.eportal.cmecgetclientdetailsrequest.CmecEPortalContactDetails;
+import uk.gov.gsi.childmaintenance.futurescheme.bo.esb.eportal.cmecgetclientdetailsrequest.CmecGetClientDetailsRequest;
+import uk.gov.gsi.childmaintenance.futurescheme.bo.esb.eportal.cmecgetclientdetailsrequest.CmecGetClientDetailsResponse;
 
 @Endpoint
 public class UserEndpoint {
@@ -33,9 +35,11 @@ public class UserEndpoint {
     @PayloadRoot(namespace = "http://www.childmaintenance.gsi.gov.uk/futurescheme/interfaces/CMECGetClientDetailsWebService",
             localPart = "getClientDetails")
     @ResponsePayload
-    public GetUserResponse getClientDetails(@RequestPayload CmecGetClientDetailsRequest request) {    											 
-        GetUserResponse response = new GetUserResponse();
-        //response.setUser(userService.getUsers(request.getName()));
+    public CmecGetClientDetailsResponse getClientDetails(@RequestPayload CmecGetClientDetailsRequest request) {    											 
+    	CmecGetClientDetailsResponse response = new CmecGetClientDetailsResponse();
+    	CmecEPortalContactDetails cmecEPortalContactDetails=  new CmecEPortalContactDetails();
+    	cmecEPortalContactDetails.setEmailAddress("a@b.com");
+    	response.setClientDetails(cmecEPortalContactDetails);
         return response;
     }
     
--- src/main/java/uk/gov/gsi/childmaintenance/futurescheme/bo/esb/eportal/cmecgetclientdetailsrequest/CmecGetClientDetailsResponse.java
+++ src/main/java/uk/gov/gsi/childmaintenance/futurescheme/bo/esb/eportal/cmecgetclientdetailsrequest/CmecGetClientDetailsResponse.java
@@ -13,6 +13,7 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -45,6 +46,7 @@ import javax.xml.bind.annotation.XmlType;
     "clientAddress",
     "cientHLSCaseDetails"
 })
+@XmlRootElement(name = "getClientDetailsResponse", namespace="http://www.childmaintenance.gsi.gov.uk/futurescheme/interfaces/CMECGetClientDetailsWebService")
 public class CmecGetClientDetailsResponse {
 
     @XmlElement(name = "ResponseHeader")
